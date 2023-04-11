d71597d48570a1bd49fd327d2f06d6824e47efb1
Viewls2 <mahasamut.kh@gmail.com>
2023-04-11 17:20:15 +0700

    add:all react ex.

--- 05-react-hook-useeffect/01-useEffect/03-exercise/src/App.jsx
+++ 05-react-hook-useeffect/01-useEffect/03-exercise/src/App.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
   const [rate, setRate] = useState(0);
   const [exchange, setExchange] = useState(1);
 
-  const syncRate = () => {
+  const syncRate = () => {  // set line(4) from 0 to 30
     setRate(30);
   };
+
+const arr = [] //ยังไม่แน่ใจมีarray เปล่าไว้ทำไม
+
+useEffect(syncRate,arr)  //เรียกsynrateเลยตั้งแต่เปิดหน้ามา
 
   const handleExchangeChange = (event) => {
     const newExchange = event.target.value;
@@ -16,7 +20,7 @@ const App = () => {
   return (
     <div>
       <h1>Current Rate: {rate}</h1>
-      <button onClick={syncRate}>Sync Rate</button>
+      {/* <button onClick={syncRate}>Sync Rate</button> */}
       <input type="number" value={exchange} onChange={handleExchangeChange} />
       <h2>Calculated Exchange: {exchange * rate}</h2>
     </div>
